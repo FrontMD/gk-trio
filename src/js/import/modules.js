6cb9a26b4902018de0065399c8a570d6f884f157
FrontMD <web@mockup.digital>
2025-10-30 17:01:28 +0300

    home in progress

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -2,9 +2,11 @@
 @@include("../../blocks/modules/modals/modals.js")
 @@include("../../blocks/modules/main-burger/main-burger.js")
 @@include("../../blocks/modules/home-intro/home-intro.js")
+@@include("../../blocks/modules/category-slider/category-slider.js")
 
 document.addEventListener('DOMContentLoaded', () => {
   homeIntro()
   modalsInit()
   mainBurger()
+  categorySlider()
 })
