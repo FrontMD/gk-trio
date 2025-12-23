e30bd4a1aae608f8aee6433dc3b67308e8fdd86e
FrontMD <web@mockup.digital>
2025-12-23 16:48:34 +0300

    range

--- src/js/import/components.js
+++ src/js/import/components.js
@@ -1,11 +1,9 @@
 @@include("../../blocks/components/form/form.js")
 @@include("../../blocks/components/s-filter/s-filter.js")
 @@include("../../blocks/components/filter-field/filter-field.js")
-@@include("../../blocks/components/filter-range/filter-range.js")
 
 document.addEventListener('DOMContentLoaded', () => {
     validation()
     sFilter()
     filterField()
-    filterRange()
 })
